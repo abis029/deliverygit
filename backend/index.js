3c9bdc978cb48ed9a3624c7af8bf7d6077f00cf5
abis029 <abishnanj@gmail.com>
2024-10-04 17:02:13 +0530

    wfdgjy
    wqefr

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use('/api/delivery-person', deliveryPersonRoutes);
 const PORT = process.env.PORT || 8020;
 
 mongoose
-  .connect("mongodb+srv://abis:1234@cluster0.ozfh0ei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://Thanu:Thanu23@cluster3.hdavn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3")
   .then(() => {
     console.log(`Connected to MongoDB`);
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
